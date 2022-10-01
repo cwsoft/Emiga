dd691a170e7512499a8e72aef0e09d248911cdfa
cwsoft <info@cwsoft.de>
2022-10-01 20:08:09 +0200

    Updated Amiberry to v5.4.

--- scripts/setup/setup_amiberry.sh
+++ scripts/setup/setup_amiberry.sh
@@ -16,7 +16,7 @@ fi
 
 ## Global variables to define the Amiberry binary file to download.
 GH_URL="https://github.com/BlitterStudio/amiberry/releases/download"
-AMIBERRY_VERSION="v5.3"
+AMIBERRY_VERSION="v5.4"
 AMIBERRY_BINARY="amiberry-$AMIBERRY_VERSION-rpi4-sdl2-64bit-debian.zip"
 AMIBERRY_URL="$GH_URL/$AMIBERRY_VERSION/$AMIBERRY_BINARY"
 
